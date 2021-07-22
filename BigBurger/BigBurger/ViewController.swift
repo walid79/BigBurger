db4c1329b88f6321696a8ca0e427bebee5a71b80
walid nakbi <walidnakbi@MacBook-Pro-de-walid.local>
2021-07-22 12:47:34 +0100

    intialisation project

---
+++ BigBurger/BigBurger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BigBurger
+//
+//  Created by walid nakbi on 22/7/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BigBurger/BigBurgerTests/BigBurgerTests.swift
@@ -0,0 +1,33 @@
+//
+//  BigBurgerTests.swift
+//  BigBurgerTests
+//
+//  Created by walid nakbi on 22/7/2021.
+//
+
+import XCTest
+@testable import BigBurger
+
+class BigBurgerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
